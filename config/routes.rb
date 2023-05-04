556cc3a8ff39b94b05ff81da1dd129288ad06aae
purin <purin.git0416@gmail.com>
2023-05-04 17:19:52 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'commands#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :create]
